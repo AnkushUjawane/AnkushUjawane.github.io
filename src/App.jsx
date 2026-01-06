05cf6b12c8fb0e3936320cea7d323451c202aace
Ankush Ujawane <multiverse1808@gmail.com>
2026-01-06 20:05:23 +0530

    checking webhook working or not

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Portfolio using devops and jenkins with connecting 2 repo using cloudflare tunnel</h1>
+      <h1>Portfolio using docker, jenkins with connecting 2 repo using cloudflare tunnel</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
