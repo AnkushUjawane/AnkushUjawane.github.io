4d57186701cde6318451856e42279339eea11f6f
Ankush Ujawane <multiverse1808@gmail.com>
2026-01-10 18:58:12 +0530

    setting up homepage and adding snoweffect in background

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { Routes } from 'react-router-dom'
-import { Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import Snowfall from 'react-snowfall'
 import HomePage from './components/HomePage/home'
 import Navbar from './components/navbar/navbar.jsx'
 import './App.css'
 
 function App() {
-
   return (
-    <div>
+    <div className='app'>
+      <Snowfall style={{ position: 'fixed', width: '100vw', height: '100vh', zIndex: -1 }} snowflakeCount={300} />
       <Navbar />
       <Routes>
         <Route path='/' element={<HomePage />} />
--- src/components/HomePage/home.jsx
+++ src/components/HomePage/home.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
-
+import "./home.css"
+import AnkushPhoto from "../../assets/Images/ankush.JPG"
 function home() {
   return (
-    <div>
-      <h1>Welcome to My Portfolio</h1>
+    <div className="home">
+      <div className='content'>
+        <div className='hi'>
+          <h3>Hello, I'm</h3>
+        </div>
+        <div className='name'>
+          <h1>Ankush Ujawane</h1>
+        </div>
+        <div className='role'>
+          <h2>Software Engineer | Web Developer</h2>
+        </div>
+        <div className='home-btn'>
+          <button>Contact Me</button>
+          <button>About Us</button>
+        </div>
+      </div>
+      <div className='photo'>
+        <img src={AnkushPhoto} alt="Ankush Image" width={350} />
+      </div>
     </div>
   )
 }
