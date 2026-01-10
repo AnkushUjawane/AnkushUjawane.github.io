bea74a3a183f47c81f8079803c27309f1f87327a
Ankush Ujawane <multiverse1808@gmail.com>
2026-01-10 15:33:42 +0530

    adding navbar to website

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import IconButton from '@mui/material/IconButton'
+import Stack from '@mui/material/Stack'
+import { GitHub, LinkedIn } from '@mui/icons-material'
 import "./navbar.css"
 const navbar = () => {
   return (
@@ -27,10 +30,24 @@ const navbar = () => {
                 Skills
               </NavLink>
             </li>
+            <li className="nav-item">
+              <NavLink className="nav-link">
+                Contact Me
+              </NavLink>
+            </li>
           </ul>
         </div>
 
-        <div>Hi</div>
+        <div>
+          <Stack className='icon-menu' direction={"row"} spacing={4} alignItems={"center"}>
+            <IconButton aria-label='Github Profile' color='default' href='https://github.com/AnkushUjawane' target='_blank'>
+              <GitHub sx={{fontSize: 30}}/>
+            </IconButton>
+            <IconButton aria-label='LinkedIn Profile' color='primary' href='https://www.linkedin.com/in/ankushujawane/' target='_blank'>
+              <LinkedIn sx={{fontSize: 30}}/>
+            </IconButton>
+          </Stack>
+        </div>
       </nav>
     </div>
   )
