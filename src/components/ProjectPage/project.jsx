15705eb05571bd8c738b532a0eaad824834c6877
Ankush Ujawane <multiverse1808@gmail.com>
2026-01-15 16:06:43 +0530

    making background black

--- src/components/ProjectPage/project.jsx
+++ src/components/ProjectPage/project.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { projects } from './projectdata.js'
 import './project.css'
 
 function project() {
@@ -6,56 +7,24 @@ function project() {
     <div className='project' id='projects'>
         <h1>My Projects</h1>
         <div className='project-card'>
-            <div className='cards'>
-                <div className='project-name'>
-                    <h2>Project Name</h2>
+            {projects.map(proj => (
+                <div className='cards'>
+                    <div className='project-name'>
+                        <h2>{proj.name}</h2>
+                    </div>
+                    <div className='project-desp'>
+                        <p>{proj.description}</p>
+                    </div>
+                    <div className='project-skill'>
+                        <h4>{proj.skills[0]}</h4>
+                        <h4>{proj.skills[1]}</h4>
+                        <h4>{proj.skills[2]}</h4>
+                    </div>
+                    <div className='project-code'>
+                        <button>{proj.githubLink}</button>
+                    </div>
                 </div>
-                <div className='project-desp'>
-                    <p>Project Description</p>
-                </div>
-                <div className='project-skill'>
-                    <h4>skill1</h4>
-                    <h4>skill2</h4>
-                    <h4>skill3</h4>
-                </div>
-                <div className='project-code'>
-                    <button>Github</button>
-                </div>
-            </div>
-
-            <div className='cards'>
-                <div className='project-name'>
-                    <h2>Project Name</h2>
-                </div>
-                <div className='project-desp'>
-                    <p>Project Description</p>
-                </div>
-                <div className='project-skill'>
-                    <h4>skill1</h4>
-                    <h4>skill2</h4>
-                    <h4>skill3</h4>
-                </div>
-                <div className='project-code'>
-                    <button>Github</button>
-                </div>
-            </div>
-
-            <div className='cards'>
-                <div className='project-name'>
-                    <h2>Project Name</h2>
-                </div>
-                <div className='project-desp'>
-                    <p>Project Description</p>
-                </div>
-                <div className='project-skill'>
-                    <h4>skill1</h4>
-                    <h4>skill2</h4>
-                    <h4>skill3</h4>
-                </div>
-                <div className='project-code'>
-                    <button>Github</button>
-                </div>
-            </div>
+            ))}
         </div>
     </div>
   )
---
+++ src/components/ProjectPage/projectdata.js
@@ -0,0 +1,5 @@
+export const projects = [
+    { id: 1, name: "Project Name", description: "Project Description", skills: ["skill1", "skill3", "skill3"], githubLink: "githublink" },
+    { id: 2, name: "Project Name1", description: "Project Description", skills: ["sql", "skill2", "skill3"], githubLink: "#" },
+    { id: 3, name: "Project Name2", description: "Project Description", skills: ["skill1", "skill2", "skill3"], githubLink: "#" },
+];
