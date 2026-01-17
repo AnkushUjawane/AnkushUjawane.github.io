2e6c27e34012f5c8b7667c5ab37e76cf0d47055c
Ankush Ujawane <multiverse1808@gmail.com>
2026-01-17 14:52:54 +0530

    setting up project page

--- src/components/ProjectPage/projectdata.js
+++ src/components/ProjectPage/projectdata.js
@@ -1,5 +1,5 @@
 export const projects = [
-    { id: 1, name: "Project Name", description: "Project Description", skills: ["skill1", "skill3", "skill3"], githubLink: "githublink" },
+    { id: 1, name: "Portfolio", description: "Fake people tend to view themselves as above others; when you think about it, this explains why they don't believe honesty or loyalty to others is important. Disingenuous individuals generally behave in problematic ways while expecting others to swallow and deal with it", skills: ["skill1", "skill3", "skill3"], githubLink: "githublink" },
     { id: 2, name: "Project Name1", description: "Project Description", skills: ["sql", "skill2", "skill3"], githubLink: "#" },
     { id: 3, name: "Project Name2", description: "Project Description", skills: ["skill1", "skill2", "skill3"], githubLink: "#" },
 ];
