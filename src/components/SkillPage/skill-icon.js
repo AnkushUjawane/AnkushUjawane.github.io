3b3777a95c37c3b7f963cc1eb5c52719590577fa
Ankush Ujawane <multiverse1808@gmail.com>
2026-01-18 15:50:42 +0530

    basic setup of skill page

---
+++ src/components/SkillPage/skill-icon.js
@@ -0,0 +1,12 @@
+import { DiDocker, DiPython, DiReact, DiLinux} from 'react-icons/di';
+import { SiKubernetes, SiGit, SiNodedotjs } from "react-icons/si";
+
+export const skills = [
+    { name: "React", icon: DiReact },
+    { name: "Python", icon: DiPython },
+    { name: "Docker", icon: DiDocker },
+    { name: "Kubernetes", icon: SiKubernetes },
+    { name: "Node.js", icon: SiNodedotjs },
+    { name: "Git", icon: SiGit },
+    { name: "Linux", icon: DiLinux },
+]
--- src/components/SkillPage/skill.jsx
+++ src/components/SkillPage/skill.jsx
@@ -1,12 +1,38 @@
 import React from 'react'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import { skills } from './skill-icon';
 import './skill.css'
 
 const Skill = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 1000,
+    autoplaySpeed: 1000,
+    cssEase: "linear",
+  }
+
   return (
-    <div id='skill'>
-        <h1>Skill</h1>
+    <div id='skill' className='slider-skill'>
+      <h1>Skill</h1>
+      <Slider {...settings} className='slider'>
+        {skills.map((skill, index) => {
+          const Icon = skill.icon;
+          return(
+            <div key={index} className='skill-container'>
+              <Icon className='skill-icon'/>
+              <p>{skill.name}</p>
+            </div>
+          );
+        })}
+      </Slider>
     </div>
   )
 }
 
-export default Skill
+export default Skill;
