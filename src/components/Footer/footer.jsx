f10338dd18af3ed1350f9ab7df3cda564e5bdbc8
Ankush Ujawane <multiverse1808@gmail.com>
2026-01-24 17:51:47 +0530

    adding footer section in website

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,9 +1,54 @@
 import React from "react";
+import IconButton from '@mui/material/IconButton'
+import Stack from '@mui/material/Stack'
+import { GitHub, LinkedIn } from '@mui/icons-material'
 import "./footer.css"
 const Footer = () => {
     return (
         <div>
-            <h1>Footer</h1>
+            <footer className="footer">
+                <div className="footer-intro">
+                    <h1 className="footer-name">Ankush Ujawane</h1>
+                    <h2 className="footer-role">Software Engineer</h2>
+                </div>
+
+                <div className="footer-info">
+                    <div className="footer-contact">
+                        <h1 className="footer-heading">Contact Information</h1>
+                        <p>Email: ankushujawane18@gmail.com</p>
+                        <p>Phone: +91 7517780013</p>
+                        <p>Location: Akola, Maharashtra, India</p>
+                    </div>
+
+                    <div className="footer-connect">
+                        <h1 className="footer-heading">Connect With Me</h1>
+                        <Stack
+                            className="footer-icons"
+                            direction="row"
+                            spacing={2}
+                            alignItems="center"
+                        >
+                            <IconButton
+                                aria-label="Github Profile"
+                                href="https://github.com/AnkushUjawane"
+                                target="_blank"
+                                color="primary"
+                            >
+                                <GitHub sx={{ fontSize: 36 }} />
+                            </IconButton>
+
+                            <IconButton
+                                aria-label="LinkedIn Profile"
+                                href="https://www.linkedin.com/in/ankushujawane/"
+                                target="_blank"
+                                color="primary"
+                            >
+                                <LinkedIn sx={{ fontSize: 36 }} />
+                            </IconButton>
+                        </Stack>
+                    </div>
+                </div>
+            </footer>
         </div>
     )
 }
