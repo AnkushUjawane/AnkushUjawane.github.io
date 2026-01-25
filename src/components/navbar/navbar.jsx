b2483e3ffa7af76b8b84f6787388aafa3bd839b0
Ankush Ujawane <multiverse1808@gmail.com>
2026-01-26 00:18:18 +0530

    navbar responsiveness complete

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -7,35 +7,34 @@ const Navbar = () => {
   return (
     <div>
       <nav className='navbar'>
-        <div className='nav-group'>
-          <ul className='nav-menu'>
-            <li className="nav-item">
-              <a href="#home">Home</a>
-            </li>
-            <li className="nav-item">
-              <a href="#about">About Us</a>
-            </li>
-            <li className="nav-item">
-              <a href="#projects">Projects</a>
-            </li>
-            <li className="nav-item">
-              <a href="#skill">Skills</a>
-            </li>
-            <li className="nav-item">
-              <a href="">Contact Me</a>
-            </li>
-          </ul>
-        </div>
+        <div className='nav-inner'>
+          <div className='nav-group'>
+            <ul className='nav-menu'>
+              <li className="nav-item">
+                <a href="#home">Home</a>
+              </li>
+              <li className="nav-item">
+                <a href="#about">About Us</a>
+              </li>
+              <li className="nav-item">
+                <a href="#projects">Projects</a>
+              </li>
+              <li className="nav-item">
+                <a href="#skill">Skills</a>
+              </li>
+            </ul>
+          </div>
 
-        <div>
-          <Stack className='icon-menu' direction={"row"} spacing={4} alignItems={"center"}>
-            <IconButton aria-label='Github Profile' color='default' href='https://github.com/AnkushUjawane' target='_blank'>
-              <GitHub sx={{fontSize: 30}}/>
-            </IconButton>
-            <IconButton aria-label='LinkedIn Profile' color='primary' href='https://www.linkedin.com/in/ankushujawane/' target='_blank'>
-              <LinkedIn sx={{fontSize: 30}}/>
-            </IconButton>
-          </Stack>
+          <div>
+            <Stack className='icon-menu' direction={"row"} spacing={{xs:1, md:3}} alignItems={"center"}>
+              <IconButton aria-label='Github Profile' color='default' href='https://github.com/AnkushUjawane' target='_blank'>
+                <GitHub sx={{ fontSize: { xs: "1.3rem", md: "2rem", sm: "1.5rem" } }} />
+              </IconButton>
+              <IconButton aria-label='LinkedIn Profile' color='primary' href='https://www.linkedin.com/in/ankushujawane/' target='_blank'>
+                <LinkedIn sx={{ fontSize: { xs: "1.3rem", md: "2rem", sm: "1.5rem" } }} />
+              </IconButton>
+            </Stack>
+          </div>
         </div>
       </nav>
     </div>
